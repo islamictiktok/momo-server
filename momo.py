afdf1495d20f357062854cb2e892da3aec1c4223
islamictiktok <islamictiktok250@gmail.com>
2025-05-21 13:50:22 +0200

    Update momo.py

--- momo.py
+++ momo.py
@@ -1,15 +1,6 @@
-from flask import Flask
 import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def run_momo():
-    try:
-        exec(open("momo_core.py").read(), globals())
-        return "✅ momo.py running"
-    except Exception as e:
-        return f"❌ Error: {str(e)}"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+try:
+    exec(open("momo_core.py").read(), globals())
+except Exception as e:
+    print(f"❌ Error: {e}")
